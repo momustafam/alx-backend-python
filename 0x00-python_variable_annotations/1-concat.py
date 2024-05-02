3f3d7b723dd6e615b80f56a70f554ddb48b2b988
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-02 20:48:42 +0000

    auto pushing

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Simple module since it has only a one simple function concat(str1, str2).'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenate two strings.
+
+        Args:
+            - str1 (str): the first string
+            - str2 (str): the second string
+
+        Return: concatenated string
+    '''
+    return str1 + str2
