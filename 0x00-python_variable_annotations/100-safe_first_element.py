cd8e2b7a6e0045a00455dbeeb2e2e722ff238231
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-02 21:29:00 +0000

    auto pushing

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Advanced task 1"""
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """returns first element of list if exists"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
