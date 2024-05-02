0f27f62df3fcd5b1e9ddbea2693bc75965eca2f0
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-02 20:51:53 +0000

    auto pushing

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Simple module since it has only a one simple function floor(n).'''
+
+
+def floor(n: float) -> int:
+    '''Get the floor of a float num.
+    
+        Paramters:
+            - n (float): the float
+
+        Return: the floor of the float
+    '''
+    return int(n)
