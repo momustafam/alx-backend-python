0e34c63e5c66159d3a77a9ed51f3a3d64e52d57c
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-02 20:35:13 +0000

    auto pushing

---
+++ 0x00-python-variable-annotations/0-add.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+'''A simple module since it has a simple function add(a, b).'''
+
+
+def add(a: float, b: float) -> float:
+    '''Add two numbers.
+
+    Parameters:
+        - a (float): the first number
+        - b (float): the second number
+
+    Return: Summation of a and b
+    '''
+    return a + b
