7cc506e0e82b688492492592dfe114caa9f8358d
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-07 21:27:08 +0000

    auto pushing

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Simple module since it has a simple function.'''
+from random import random
+import asyncio
+
+
+async def async_generator():
+    '''coroutine yield a random number between 0 and 10.'''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random()*10
