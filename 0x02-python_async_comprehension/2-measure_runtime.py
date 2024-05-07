0ed389fec2145ad8fa4a6434c33a33d67e3eb7f6
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-07 21:43:37 +0000

    auto pushing

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,13 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 '''Simple module since it has a simple funciton.'''
 import asyncio
 from time import time
 
-comp = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     '''measure total time of running async function 4 times and return it.'''
     start_time = time()
-    await asyncio.gather(comp(), comp(), comp(), comp())
+    await asyncio.gather(*[async_comprehension() for i in range(4)])
     return time() - start_time
